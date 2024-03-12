3d19e218efec33b83402560a89cba188ea9d675b
Christian Daguerre <cdaguerre@worldia.com>
2024-03-12 11:34:27 +0100

    add ability to customize rules

--- src/Factory.php
+++ src/Factory.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the platform/coding-standards package.
+ * This file is part of the worldia/coding-standards package.
  * (c) Worldia <developers@worldia.com>
  */
 
@@ -15,14 +15,21 @@ class Factory
 {
     public static function createPhpCsFixerConfig(string $rootDirectory, array $options = []): PhpCsFixer\Config
     {
+        $package = json_decode(file_get_contents($rootDirectory.'/composer.json'), true);
+        $header = sprintf("This file is part of the %s package.\n(c) Worldia <developers@worldia.com>", $package['name']);
+
         $defaults = [
             'dirs' => ['lib', 'vendor', 'var'],
+            'rules' => [
+                '@PSR12' => true,
+                '@Symfony' => true,
+                '@Symfony:risky' => true,
+                'single_line_throw' => false,
+                'header_comment' => ['header' => $header],
+            ],
         ];
 
         $config = array_merge_recursive($defaults, $options);
-
-        $package = json_decode(file_get_contents($rootDirectory.'/composer.json'), true);
-        $header = sprintf("This file is part of the %s package.\n(c) Worldia <developers@worldia.com>", $package['name']);
 
         $finder = (new PhpCsFixer\Finder())
             ->exclude($config['dirs'])
@@ -31,13 +38,7 @@ class Factory
         return (new PhpCsFixer\Config())
             ->setUsingCache(false)
             ->setRiskyAllowed(true)
-            ->setRules([
-                '@PSR12' => true,
-                '@Symfony' => true,
-                '@Symfony:risky' => true,
-                'single_line_throw' => false,
-                'header_comment' => ['header' => $header],
-            ])
+            ->setRules($config['rules'])
             ->setFinder($finder);
     }
 }
